6523f4596ba7bcc3cd58abe6f8e82beb23e27d88
jebb king <gabouchard2002@gmail.com>
2024-02-17 21:39:01 +0300

    revise exception errors

--- math_module.py
+++ math_module.py
@@ -1,24 +1,17 @@
+import math
 from math import *
 
 def my_div(a, b):
     try:
-        res = a / b
-        return int(res)
+        res = int(a) / int(b)
+        return round(res, 2)
     except ZeroDivisionError:
         return "You can`t divide by zero!"
-    except TypeError:
-        return "You entered the wrong argument."
 
 def my_sum(a, b):
-    try:
-        res = a + b
-        return res
-    except TypeError:
-        return "Wrong argument."
+    res = int(a) + int(b)
+    return res
 
 def my_sub(a, b):
-    try:
-        res = a - b
-        return res
-    except TypeError:
-        print('Wrong argument.')
+    res = int(a) - int(b)
+    return math.ceil(res)
