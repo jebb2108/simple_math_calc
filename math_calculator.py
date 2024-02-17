2d4986cb40654edf9b656f2b7a37b1fdd3fad3e5
jebb king <gabouchard2002@gmail.com>
2024-02-17 21:31:13 +0300

    add interaction

--- math_calculator.py
+++ math_calculator.py
@@ -1,13 +1,18 @@
 from math_module import my_div, my_sum, my_sub
 
-print('Here are the results of your calculations: ')
+print("Give me two numbers in this format [a b], and I`ll do you math!")
 
 def main():
-    div = my_div(6, 3)
-    sum = my_sum(4, 2)
-    sub = my_sub(60, 28)
-    return print(sum, div, sub)
+    sum = my_sum(sum_a, sum_b)
+    sub = my_sub(sub_a, sub_b)
+    div = my_div(div_a, div_b)
+    return print(f"Here are the result of adding: {sum}, "
+                 f"substraction: {sub}, and division: {div}")
 
+sum_a, sum_b = input('Two numbers to add up: ').split(' ')
+sub_a, sub_b = input('Two numbers to substract: ').split(' ')
+div_a, div_b = input('Two numbers to divide: ').split(' ')
 
+# launching the programm.
 if __name__ == '__main__':
     main()
--- math_module.py
+++ math_module.py
@@ -2,8 +2,8 @@ from math import *
 
 def my_div(a, b):
     try:
-        res = a / b
-        return int(res)
+        res = int(a) / int(b)
+        return round(res, 2)
     except ZeroDivisionError:
         return "You can`t divide by zero!"
     except TypeError:
@@ -11,14 +11,14 @@ def my_div(a, b):
 
 def my_sum(a, b):
     try:
-        res = a + b
-        return res
+        res = int(a) + int(b)
+        return int(res)
     except TypeError:
         return "Wrong argument."
 
 def my_sub(a, b):
     try:
-        res = a - b
-        return res
+        res = int(a) - int(b)
+        return int(res)
     except TypeError:
         print('Wrong argument.')
