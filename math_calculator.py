1a23052e47cd72a3692e672beee8b536aecf06a2
jebb king <gabouchard2002@gmail.com>
2024-02-17 20:33:41 +0300

    add some features

--- math_calculator.py
+++ math_calculator.py
@@ -1,10 +1,12 @@
-from math_module import check_division, my_sum
+from math_module import check_division, my_sum, my_sub
 
+print('Here are the results of your calculations: ')
 
 def main():
     division = check_division(6, 3)
     sum = my_sum(4, 2)
-    return print(sum, division)
+    sub = my_sub(60, 28)
+    return print(sum, division, sub)
 
 
 if __name__ == '__main__':
