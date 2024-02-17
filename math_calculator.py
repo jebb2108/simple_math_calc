8429fdf8ffc77d3463f48a8d7b785055dd326b88
jebb king <gabouchard2002@gmail.com>
2024-02-17 22:13:52 +0300

    release v1.1

--- math_calculator.py
+++ math_calculator.py
@@ -1,14 +1,5 @@
 from math_module import my_div, my_sum, my_sub
 
-print('Here are the results of your calculations:\n')
-
-def main():
-    div = my_div(6, 3)
-    sum = my_sum(4, 2)
-    sub = my_sub(60, 28)
-    return print(f"The result of adding: {sum}, "
-                 f"division: {div}, substraction: {sub}")
-
 print("Give me two numbers in this format [a b], and I`ll do you math!")
 
 def main():
