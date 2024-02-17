512fc500d09e34fa9de2d57d1504dccfb5983410
jebb king <gabouchard2002@gmail.com>
2024-02-17 21:00:18 +0300

    Making it look nicer

--- math_calculator.py
+++ math_calculator.py
@@ -1,12 +1,13 @@
 from math_module import my_div, my_sum, my_sub
 
-print('Here are the results of your calculations: ')
+print('Here are the results of your calculations:\n')
 
 def main():
     div = my_div(6, 3)
     sum = my_sum(4, 2)
     sub = my_sub(60, 28)
-    return print(sum, div, sub)
+    return print(f"The result of adding: {sum}, "
+                 f"division: {div}, substraction: {sub}")
 
 
 if __name__ == '__main__':
